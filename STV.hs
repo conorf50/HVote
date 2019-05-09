5ff0acae45c8237729616b19a400446cbecdfbca
Conor <conorf50@gmail@.com>
2019-05-09 04:44:02 +0100

    moved voting code to module

---
+++ STV.hs
@@ -0,0 +1,30 @@
+module STV (
+    concatBallots,
+    countLetters,
+    cntVotes,
+    droopQuota
+)
+where
+import Ballots
+
+
+-- convert the lists of votes into one long String (based on examples from notes)
+concatBallots :: [[a]] -> [a]
+concatBallots xss = [x | xs <- xss, x <- xs]
+
+-- count how many votes each candidate got
+-- source: https://stackoverflow.com/questions/19297059/count-number-of-instances-of-char-in-a-given-string-haskell
+countLetters :: String -> Char -> Int
+countLetters str c = length $ filter (== c) str
+
+-- for each of of our candidates, count their votes
+-- find occurances matcing the character c
+cntVotes :: Char -> Int
+cntVotes c = countLetters (concatBallots ballots) c
+
+
+-- calculate the quota based on the 'droop quota' method
+-- also floors the values too
+droopQuota :: Int -> Int -> Int
+droopQuota 0 0 = 0
+droopQuota x c = x`div`(c+1)+1
--- cfarrell-assign.hs
+++ cfarrell-assign.hs
@@ -7,6 +7,7 @@
 -- some sample votes for our candidates A,B,C,D and E
 -- imported from Ballots.hs module
 import Ballots
+import STV
 
 
 toElect :: Int
@@ -15,32 +16,6 @@ toElect = 3
 
 totalBallots :: Int
 totalBallots = length ballots
-
-getVoterPreferences :: [[String]] -> [Char]
-getVoterPreferences = undefined
-
-
-
--- convert the lists of votes into one long String (based on examples from notes)
-concatBallots :: [[a]] -> [a]
-concatBallots xss = [x | xs <- xss, x <- xs]
-
--- count how many votes each candidate got
--- source: https://stackoverflow.com/questions/19297059/count-number-of-instances-of-char-in-a-given-string-haskell
-countLetters :: String -> Char -> Int
-countLetters str c = length $ filter (== c) str
-
--- for each of of our candidates, count their votes
--- find occurances matcing the character c
-cntVotes :: Char -> Int
-cntVotes c = countLetters (concatBallots ballots) c
-
-
--- calculate the quota based on the 'droop quota' method
--- also floors the values too
-droopQuota :: Int -> Int -> Int
-droopQuota 0 0 = 0
-droopQuota x c = x`div`(c+1)+1
 
 
 
