c5d0aeeeea47250efe10c47b93f0d6fa00625712
Conor <conorf50@gmail@.com>
2019-05-09 03:42:00 +0100

    added quota calculation and seat selection

--- cfarrell-assign.hs
+++ cfarrell-assign.hs
@@ -8,6 +8,11 @@
 -- imported from Ballots.hs module
 import Ballots
 
+
+toElect :: Int
+toElect = 3
+
+
 totalBallots :: Int
 totalBallots = length ballots
 
@@ -25,23 +30,40 @@ concatBallots xss = [x | xs <- xss, x <- xs]
 countLetters :: String -> Char -> Int
 countLetters str c = length $ filter (== c) str
 
--- for each of of our candidates, rank their votes
+-- for each of of our candidates, count their votes
 -- find occurances matcing the character c
 cntVotes :: Char -> Int
 cntVotes c = countLetters (concatBallots ballots) c
 
 
+-- calculate the quota based on the 'droop quota' method
+-- also floors the values too
+droopQuota :: Int -> Int -> Int
+droopQuota 0 0 = 0
+droopQuota x c = x`div`(c+1)+1
+
+
+
 main :: IO()
 main = do
+    -- how many candidates do we have to elect?
+    putStrLn "How many candidates have to be elected?"
+    t <- getLine
+    -- parse this as an Int
+    let toElect = (read t :: Int)
+
+    putStrLn ("Will elect " ++ show(toElect) ++ " candidates")
     -- call other stuff here
 
+    -- print out a seperator
+    putStrLn "----------------------------------"
     -- print without new line
     putStr "Total amount of ballots cast: "
     print(totalBallots)
 
     --print(cntVotes ([ x | x <- ['A'.. 'F']]))
 
-
+    -- re-do this as recursive function
     putStr "Total amount of votes for candidate 'A': "
     print(cntVotes 'A')
 
@@ -57,6 +79,8 @@ main = do
     putStr "Total amount of votes for candidate 'E': "
     print(cntVotes 'E')
 
-
-
+    -- print out a seperator
+    putStrLn "----------------------------------"
     
+    putStr ("Calculted quota for "  ++ show(totalBallots) ++ " ballots and "++ show(toElect) ++ " candidates : ")
+    print(droopQuota totalBallots toElect)
